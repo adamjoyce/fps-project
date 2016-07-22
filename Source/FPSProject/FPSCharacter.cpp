be1a21c10bd5a9bbbfeed319b4f07cc88298b075
Adam Joyce <adamjoyce@outlook.com>
2016-07-22 20:29:43 +0100

    Adjusted muzzle pitch skew

--- Source/FPSProject/FPSCharacter.cpp
+++ Source/FPSProject/FPSCharacter.cpp
@@ -118,7 +118,7 @@ void AFPSCharacter::OnFire()
         FVector const MuzzleLocation = CameraLoc + FTransform(CameraRot).TransformVector(MuzzleOffset);
         FRotator MuzzleRotation = CameraRot;
         // Skew the aim upwards.
-        MuzzleRotation.Pitch += 10.0f;
+        MuzzleRotation.Pitch += 5.75f;
 
         UWorld* const World = GetWorld();
         if (World)
